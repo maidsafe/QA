29d8933708d91d6024ea92fb6d9ab97954d04a93
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-07-10 11:43:27 +0100

    Added install script for libsodium for Travis' use.

---
+++ Bash Scripts/Travis/install_libsodium.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.3/libsodium-1.0.3.tar.gz
+tar xvfz libsodium-1.0.3.tar.gz
+cd libsodium-1.0.3
+./configure --prefix=/usr --enable-shared=no
+CORES=$(nproc)
+make check -j$CORES
+make install
