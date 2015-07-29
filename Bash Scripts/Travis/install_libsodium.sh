25d176fcbda381b6a1668eaad2ed7096e84a821d
maqi <qi.ma@maidsafe.net>
2015-07-29 11:27:01 +0100

    travis OSX remove core check (not supported in OSX)

--- Bash Scripts/Travis/install_libsodium.sh
+++ Bash Scripts/Travis/install_libsodium.sh
@@ -3,9 +3,8 @@ set -e
 wget https://github.com/jedisct1/libsodium/releases/download/1.0.3/libsodium-1.0.3.tar.gz
 tar xvfz libsodium-1.0.3.tar.gz
 cd libsodium-1.0.3
-./configure --prefix=$HOME/libsodium-1.0.3 --enable-shared=no
-CORES=$(nproc)
-make check -j$CORES
+./configure --prefix=$HOME/libsodium-1.0.3
+make check
 make install
 export LIBRARY_PATH=$LIBRARY_PATH:$HOME/libsodium-1.0.3/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/libsodium-1.0.3/lib
