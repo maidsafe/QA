8e3e09713702fc705e2472656fb7022f8dab2b60
maqi <qi.ma@maidsafe.net>
2015-07-29 12:47:33 +0100

    Update install_libsodium.sh using libsodium-1.0.3

--- Bash Scripts/Travis/install_libsodium.sh
+++ Bash Scripts/Travis/install_libsodium.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget https://github.com/jedisct1/libsodium/releases/download/1.0.0/libsodium-1.0.0.tar.gz
-tar xvfz libsodium-1.0.0.tar.gz
-cd libsodium-1.0.0
-./configure --prefix=$HOME/libsodium-1.0.0
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.3/libsodium-1.0.3.tar.gz
+tar xvfz libsodium-1.0.3.tar.gz
+cd libsodium-1.0.3
+./configure --prefix=$HOME/libsodium-1.0.3 --enable-shared=no
 make check
 make install
 cd ..
