f803794c1bcafa7b236acec9b77662c2080de322
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-08-11 16:04:42 +0100

    Added yum repo to install script.

--- Bash Scripts/create_linux_vault_package.sh
+++ Bash Scripts/create_linux_vault_package.sh
@@ -7,13 +7,14 @@ trap 'exit' ERR
 function help {
   echo "
 This invokes the 'create_package.sh' script in the SAFE Vault project which
-builds the linux packages.  It then copies them to the apt server.
+builds the linux packages.  It then copies them to the apt and yum servers.
 
 You should either invoke this script from the root of the safe_vault repo, or
 pass a single arg to this script which is the absolute path to the safe_vault
 repo.
 
-You must be able to ssh to the apt server without the need for a password.
+Ideally, you should be able to ssh to the apt and yum servers without the need
+for a password.
 
 Example usage:
     ./${0##*/} \"/home/me/SuperProject/SAFE Vault\"
@@ -42,6 +43,9 @@ fi
 # Invoke the script and scp the resulting packages
 "$CreatePackageScript"
 ssh apt.maidsafe.net 'mkdir -p ~/systemd/ && mkdir -p ~/SysV-style/'
+ssh yum.maidsafe.net 'mkdir -p ~/systemd/ && mkdir -p ~/SysV-style/'
 scp "$VaultRoot"/packages/linux/safe_vault_*.tar.gz apt.maidsafe.net:~/
-scp "$VaultRoot"/packages/linux/systemd/safe* apt.maidsafe.net:~/systemd/
-scp "$VaultRoot"/packages/linux/SysV-style/safe* apt.maidsafe.net:~/SysV-style/
+scp "$VaultRoot"/packages/linux/systemd/safe*.deb apt.maidsafe.net:~/systemd/
+scp "$VaultRoot"/packages/linux/SysV-style/safe*.deb apt.maidsafe.net:~/SysV-style/
+scp "$VaultRoot"/packages/linux/systemd/safe*.rpm yum.maidsafe.net:~/systemd/
+scp "$VaultRoot"/packages/linux/SysV-style/safe*.rpm yum.maidsafe.net:~/SysV-style/
