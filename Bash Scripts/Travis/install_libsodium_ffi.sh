d7df71af2f424b1947069d74ac681c4b913f0c85
Viv Rajkumar <viv.rajkumar@maidsafe.net>
2015-09-01 19:56:35 +0100

    ffi script cleanup

--- Bash Scripts/Travis/install_libsodium_ffi.sh
+++ Bash Scripts/Travis/install_libsodium_ffi.sh
@@ -5,7 +5,5 @@ cd libsodium-1.0.3
 ./configure --prefix=$HOME/libsodium-1.0.3
 make check
 make install
-ln -s /home/travis/libsodium-1.0.3/lib/libsodium.so /home/travis/libsodium-1.0.3/lib/libsodium.so.13
 export LD_LIBRARY_PATH=/home/travis/libsodium-1.0.3/lib:$LD_LIBRARY_PATH
-ls -al /home/travis/libsodium-1.0.3/lib
 cd ..
