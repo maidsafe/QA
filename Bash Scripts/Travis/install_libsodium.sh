32cef304d170484fa3cad35d52fe2eaaf3e7b32e
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-09-03 12:43:48 +0100

    Updated install_libsodium script.

--- Bash Scripts/Travis/install_libsodium.sh
+++ Bash Scripts/Travis/install_libsodium.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-wget https://github.com/jedisct1/libsodium/releases/download/1.0.3/libsodium-1.0.3.tar.gz
-tar xvfz libsodium-1.0.3.tar.gz
-cd libsodium-1.0.3
-./configure --prefix=$HOME/libsodium-1.0.3 --enable-shared=no --disable-pie
-make check
+
+LibSodiumVersion=1.0.3
+
+# Stop the script if any command fails
+set -o errtrace
+trap 'exit' ERR
+
+wget https://github.com/jedisct1/libsodium/releases/download/$LibSodiumVersion/libsodium-$LibSodiumVersion.tar.gz
+tar xfz libsodium-$LibSodiumVersion.tar.gz
+cd libsodium-$LibSodiumVersion
+./configure --prefix=$HOME/libsodium-$LibSodiumVersion --enable-shared=no --disable-pie
+Cores=$((hash nproc 2>/dev/null && nproc) || (hash sysctl 2>/dev/null && sysctl -n hw.ncpu) || echo 1)
+make check -j$Cores
 make install
-export PKG_CONFIG_PATH=$HOME/libsodium-1.0.3/lib/pkgconfig:$PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=$HOME/libsodium-$LibSodiumVersion/lib/pkgconfig:$PKG_CONFIG_PATH
 cd ..
