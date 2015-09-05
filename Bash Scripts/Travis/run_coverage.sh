12f605bfac3aaac50cee56b557616ad8634ede85
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-09-05 14:41:43 +0100

    Split single Travis script into one for deploying the docs and another for running coverage.

---
+++ Bash Scripts/Travis/run_coverage.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Show expanded commands while running
+set -x
+
+# Stop the script if any command fails
+set -o errtrace
+trap 'exit' ERR
+
+cd $HOME
+
+# Check to see if elfutils dir has been retrieved from cache
+ElfUtilsVersion=0.163
+ElfUtilsInstallPath=$HOME/elfutils-$ElfUtilsVersion
+Cores=$((hash nproc 2>/dev/null && nproc) || (hash sysctl 2>/dev/null && sysctl -n hw.ncpu) || echo 1)
+if [ ! -d "$ElfUtilsInstallPath/lib" ]; then
+  # If not, build and install it
+  mkdir -p temp
+  cd temp
+  wget https://fedorahosted.org/releases/e/l/elfutils/$ElfUtilsVersion/elfutils-$ElfUtilsVersion.tar.bz2
+  tar jxf elfutils-$ElfUtilsVersion.tar.bz2
+  cd elfutils-$ElfUtilsVersion
+  ./configure --prefix=$ElfUtilsInstallPath
+  make check -j$Cores
+  make install
+else
+  echo "Using cached elfutils directory."
+fi
+export LD_LIBRARY_PATH=$ElfUtilsInstallPath/lib:$LD_LIBRARY_PATH
+
+cd $HOME
+
+# Build and install kcov (which is fast and not versioned, so there's little point in caching it)
+mkdir -p temp
+cd temp
+wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
+tar xzf master.tar.gz
+cmake -Bbuild_kcov -Hkcov-master -DCMAKE_INSTALL_PREFIX=$HOME/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INCLUDE_PATH="$ElfUtilsInstallPath/include" -DCMAKE_LIBRARY_PATH="$ElfUtilsInstallPath/lib"
+cd build_kcov
+make -j$Cores
+make install
+
+# Build the project's tests and run them under kcov
+cd $TRAVIS_BUILD_DIR
+cargo test --no-run
+ProjectName=${TRAVIS_REPO_SLUG##*/};
+$HOME/bin/kcov --coveralls-id=$TRAVIS_JOB_ID --include-path=src target/kcov target/debug/$ProjectName-*
