b08098f1a29afd92140a6a59b4c14bb0b265af87
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-10-23 09:48:29 +0100

    Bugfix in Powershell script.

--- Powershell Scripts/AppVeyor/Install MinGW.ps1
+++ Powershell Scripts/AppVeyor/Install MinGW.ps1
@@ -3,5 +3,5 @@ if($env:platform -eq 'x86') {
 } else {
     Start-FileDownload "http://libgd.blob.core.windows.net/mingw/mingw-w64-dgn-x86_64-20141001.7z";
     7z x -oC:\ mingw-w64-dgn-x86_64-20141001.7z | findstr /b /c:"Everything is Ok" /c:"Scanning" /c:"Creating archive";
-    $env:Path = "C:\MinGW64\bin" + $env:Path;
+    $env:Path = "C:\MinGW64\bin;" + $env:Path;
 }
