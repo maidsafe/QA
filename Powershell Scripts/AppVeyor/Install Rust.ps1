b799511b639e9aa8ec8a0474e82034dcd19b49f0
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-11-11 09:50:58 +0000

    Back to latest nightly for AppVeyor.

--- Powershell Scripts/AppVeyor/Install Rust.ps1
+++ Powershell Scripts/AppVeyor/Install Rust.ps1
@@ -14,11 +14,7 @@ if ($env:RUST_VERSION -eq "stable") {
 $rust_install = "rust-$rust_version-$arch-pc-windows-gnu.msi"
 
 # Download Rust installer
-if ($env:RUST_VERSION -eq "nightly") {
-    Start-FileDownload "http://static.rust-lang.org/dist/2015-11-01/$rust_install" -FileName $rust_install
-} else {
-    Start-FileDownload "https://static.rust-lang.org/dist/$rust_install" -FileName $rust_install
-}
+Start-FileDownload "https://static.rust-lang.org/dist/$rust_install" -FileName $rust_install
 
 # Install Rust
 Start-Process -FilePath msiexec -ArgumentList /i, $rust_install, /quiet, INSTALLDIR="C:\Rust" -Wait
