b59dee473799cd3610292ce15defe3c690ee3982
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-01-21 11:09:25 +0000

    Avoid losing old version docs when publishing new docs.

--- Bash Scripts/Travis/deploy_docs.sh
+++ Bash Scripts/Travis/deploy_docs.sh
@@ -17,7 +17,15 @@ pip install --user ghp-import
 CommitMessage=$(git log -1 | tr '[:upper:]' '[:lower:]' | grep "version change to " | tr -d ' ')
 git config --global user.email qa@maidsafe.net
 git config --global user.name MaidSafe-QA
-mkdir -p docs-stage
+
+# If gh-pages branch already exists, clone it to pull down the existing version-specific docs.
+# Otherwise just create an empty folder, since this is the first push to gh-pages.
+if [ git rev-parse --verify origin/gh-pages > /dev/null 2>&1 ]; then
+  git clone https://github.com/${TRAVIS_REPO_SLUG}.git --branch gh-pages --single-branch docs-stage
+else
+  mkdir -p docs-stage
+fi
+
 cd docs-stage
 echo "<meta http-equiv=refresh content=0;url=master/${ProjectName}/index.html>" > index.html
 rm -rf .git*
