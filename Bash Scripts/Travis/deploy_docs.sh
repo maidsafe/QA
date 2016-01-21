64a84df3081463e4548a4351213c47c1e6cd0dea
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-01-21 16:45:51 +0000

    Another bugfix to Travis script.

--- Bash Scripts/Travis/deploy_docs.sh
+++ Bash Scripts/Travis/deploy_docs.sh
@@ -20,6 +20,8 @@ git config --global user.name MaidSafe-QA
 
 # If gh-pages branch already exists, clone it to pull down the existing version-specific docs.
 # Otherwise just create an empty folder, since this is the first push to gh-pages.
+git remote set-branches origin '*'
+git fetch
 if git rev-parse --verify origin/gh-pages > /dev/null 2>&1; then
   git clone https://github.com/${TRAVIS_REPO_SLUG}.git --branch gh-pages --single-branch docs-stage
 else
