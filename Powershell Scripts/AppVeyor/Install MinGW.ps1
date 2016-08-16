90fa8fd785aa1b721f458f9c9b97c63cf749450f
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-08-16 08:41:45 +0100

    Some small housekeeping tasks.

--- Powershell Scripts/AppVeyor/Install MinGW.ps1
+++
@@ -1,7 +0,0 @@
-if($env:platform -eq 'x86') {
-    $env:Path = "C:\MinGW\bin;" + $env:Path;
-} else {
-    Start-FileDownload "http://libgd.blob.core.windows.net/mingw/mingw-w64-dgn-x86_64-20141001.7z";
-    7z x -oC:\ mingw-w64-dgn-x86_64-20141001.7z | findstr /b /c:"Everything is Ok" /c:"Scanning" /c:"Creating archive";
-    $env:Path = "C:\MinGW64\bin;" + $env:Path;
-}
--- Powershell Scripts/AppVeyor/Install libsodium.ps1
+++
@@ -1,2 +0,0 @@
-Start-FileDownload "https://github.com/maidsafe/QA/raw/master/Dependencies/Windows/$env:platform/libsodium.a";
-$env:SODIUM_LIB_DIR = Resolve-Path .
