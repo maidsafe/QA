dfc65aec9fc82a6c0d02382da9786f7b99fb085b
QA <qa@maidsafe.net>
2019-02-15 09:58:40 +0000

    Updated benchmark and soak scripts.

--- soak_bench_scripts/benchmark.sh
+++ soak_bench_scripts/benchmark.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#set -e
 
 date
 
@@ -7,6 +8,7 @@ date
 #find /tmp/parsec_graphs -maxdepth 1 -type d -ctime 1 -exec rm -r "{}" \;
 
 cd /home/maidsafe/Projects/benchmarks/parsec
+git checkout master
 git pull
 git log -1 --oneline
 /home/maidsafe/.cargo/bin/cargo update
@@ -33,10 +35,15 @@ NOT_CHANGED=$(grep -c 'detected' $LOGFILE)
 NOISE=$(grep -c 'noise' $LOGFILE)
 REGRESSED=$(grep -c 'regressed' $LOGFILE) 
 
-# Print test results and append to logfile
-printf "No. of tests that have improved: %i\n\n" $IMPROVED | tee -a $LOGFILE
-printf "No. of tests that have not changed: %i\n\n" $NOT_CHANGED | tee -a $LOGFILE
-printf "No of tests that have changed within noise threshold: %i\n\n" $NOISE | tee -a $LOGFILE
-printf "No. of tests that have regressed: %i\n\n" $REGRESSED | tee -a $LOGFILE
+# Master log to track daily changes in test results
+MASTERLOG="/home/maidsafe/Projects/benchmarks/parsec/masterlog.log"
+touch $MASTERLOG
+date -d "today" +"%Y%m%d%H%M" | tee -a $MASTERLOG
+
+# Print test results and append to logfile and masterlog
+printf "No. of tests that have improved: %i\n\n" $IMPROVED | tee -a $LOGFILE -a $MASTERLOG
+printf "No. of tests that have not changed: %i\n\n" $NOT_CHANGED | tee -a $LOGFILE -a $MASTERLOG
+printf "No of tests that have changed within noise threshold: %i\n\n" $NOISE | tee -a $LOGFILE -a $MASTERLOG
+printf "No. of tests that have regressed: %i\n\n" $REGRESSED | tee -a $LOGFILE -a $MASTERLOG
 
 printf "==================================================================\n\n"
--- soak_bench_scripts/soak.sh
+++ soak_bench_scripts/soak.sh
@@ -3,16 +3,18 @@
 # Cargo operations, including removing artifcats from previous builds and building the binary needed for the soak testing
 # Git pull to ensure code is up to date
 # Output to log.log
-cargo clean
-git pull
-cargo update
-cargo test --release --features=testing
+LOGFILE="/home/maidsafe/Projects/parsec/log.log"
+
+cargo clean | tee $LOGFILE
+git pull | tee -a $LOGFILE
+cargo update | tee -a $LOGFILE
+cargo test --release --features=testing | tee -a $LOGFILE
 
 #remove the build artefact file with same name as integration test we want:
 rm target/release/integration_tests-*.d
 
-echo "Starting soak tests..."
+printf "Starting soak tests...\n"
 
-#running fromm parsec folder - will need full path
-#this command will loop the test forever until it hits an error
-loop -q target/release/integration_tests-* | tee /home/maidsafe/Projects/parsec/log.log 2>&1
+# Running fromm parsec folder - will need full path
+# This command will loop the test forever until it hits an error
+loop -q target/release/integration_tests-* | tee -a $LOGFILE
