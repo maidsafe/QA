326f7f83f130a64940e8aa5bd7b7c40bbb4c3595
Calum Craig <calum.craig@maidsafe.net>
2019-04-03 14:25:22 +0100

    chore/update soak script

--- soak_bench_scripts/soak.sh
+++ soak_bench_scripts/soak.sh
@@ -3,12 +3,14 @@
 # Cargo operations, including removing artifcats from previous builds and building the binary needed for the soak testing
 # Git pull to ensure code is up to date
 # Output to log.log
-LOGFILE="/home/maidsafe/Projects/parsec/log.log"
+# Generate stack trace
+LOGFILE="/home/maidsafe/Projects/soak/parsec/log.log"
+export RUST_BACKTRACE=1
 
-cargo clean | tee $LOGFILE
-git pull | tee -a $LOGFILE
-cargo update | tee -a $LOGFILE
-cargo test --release --features=testing | tee -a $LOGFILE
+cargo clean |& tee -a $LOGFILE
+git pull |& tee -a $LOGFILE
+cargo update |& tee -a $LOGFILE
+cargo test --release --features=testing |& tee -a $LOGFILE
 
 #remove the build artefact file with same name as integration test we want:
 rm target/release/integration_tests-*.d
@@ -17,4 +19,4 @@ printf "Starting soak tests...\n"
 
 # Running fromm parsec folder - will need full path
 # This command will loop the test forever until it hits an error
-loop -q target/release/integration_tests-* | tee -a $LOGFILE
+loop -q target/release/integration_tests-* |& tee -a -i --output-error=warn $LOGFILE
