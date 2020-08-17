944a86e0796ca5fef16d628385cb22cb0c1b51c3
Stephen Coyle <stephen.coyle@maidsafe.net>
2020-08-17 14:14:47 +0100

    chode(script): add safe-authd version selection to script

--- misc-scripts/start-local-section.sh
+++ misc-scripts/start-local-section.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-echo "Running new SAFE Network section with $1 vault and $2 cli"
+echo "Running new SAFE Network section with $1 vault, $2 cli, and $3 authd"
 echo ""
 
 # Download the specified vault version 
@@ -15,21 +15,36 @@ chmod +x $HOME/.safe/vault/safe_vault
 wget "https://safe-api.s3.eu-west-2.amazonaws.com/safe-cli-$2-x86_64-unknown-linux-gnu.tar.gz" -O ./safe.tar.gz;
 
 ls .
-mkdir -p $HOME/.safe/safe-cli
-mkdir -p $HOME/.safe/authd
-tar -xvzf ./safe.tar.gz -C $HOME/.safe/safe-cli/
-PATH=$HOME/.safe/safe-cli:$PATH
-PATH=$HOME/.safe/authd:$PATH
-chmod +x $HOME/.safe/safe-cli/safe
+mkdir -p $HOME/.safe/cli
+tar -xvzf ./safe.tar.gz -C $HOME/.safe/cli/
+echo ""
+echo "List contents of $HOME/.safe/cli/ :"
+ls $HOME/.safe/cli/
+PATH=$HOME/.safe/cli:$PATH
+chmod +x $HOME/.safe/cli/safe
+echo ""
+echo "PATH:"
+echo $PATH
 
-# Install safe authd   
-safe auth install
+# Download the specified authd version
+wget "https://safe-api.s3.eu-west-2.amazonaws.com/safe-authd-$3-x86_64-unknown-linux-gnu.tar.gz" -O ./safe-authd.tar.gz; 
+
+ls .
+mkdir -p $HOME/.safe/authd
+tar -xvzf ./safe-authd.tar.gz -C $HOME/.safe/authd/
+echo ""
+echo "List contents of $HOME/.safe/authd/ :"
+ls $HOME/.safe/authd/
+PATH=$HOME/.safe/authd:$PATH
 chmod +x $HOME/.safe/authd/safe-authd
+echo ""
+echo "PATH:"
+echo $PATH
 
 # Check versions
 $HOME/.safe/authd/safe-authd -V
 $HOME/.safe/vault/safe_vault -V
 safe --version
 
-# Start Baby Fleming Network
+# Start section
 safe vault run-baby-fleming
